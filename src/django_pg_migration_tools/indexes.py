bce23b5f2d36a48d0a4a91e32d7c3a2fede37863
Marcelo Fernandes <marceelofernandes@gmail.com>
2024-12-19 14:19:44 +1300

    fixup! Add UniqueIndex class

--- src/django_pg_migration_tools/indexes.py
+++ src/django_pg_migration_tools/indexes.py
@@ -1,12 +1,20 @@
 from typing import Any
 
+from django.db import models
 from django.db.backends import ddl_references
+from django.db.backends.base import schema as base_schema
 from django.db.models.indexes import Index
 
 
 class UniqueIndex(Index):
-    def create_sql(self, *args: Any, **kwargs: Any) -> ddl_references.Statement:
-        statement = super().create_sql(*args, **kwargs)
+    def create_sql(
+        self,
+        model: type[models.Model],
+        schema_editor: base_schema.BaseDatabaseSchemaEditor,
+        using: str = "",
+        **kwargs: Any,
+    ) -> ddl_references.Statement:
+        statement = super().create_sql(model, schema_editor, using, **kwargs)
         statement.template = statement.template.replace(
             "CREATE INDEX", "CREATE UNIQUE INDEX"
         )
