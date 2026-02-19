c4a07480fa2f8de8d15b2d17250cbca8b175b93b
James Beith <james@beith.co.uk>
2026-02-19 11:02:51 +1100

    Drop Django 4.2 and 5.0 versions from the testing matrix
    These versions, as well as 5.1, have reached end-of-life.

--- noxfile.py
+++ noxfile.py
@@ -48,8 +48,6 @@ def temp_lock_file() -> Generator[IO[str], None, None]:
 @nox.parametrize(
     "django_version",
     [
-        nox.param("4.2", id="django~=4.2"),
-        nox.param("5.0", id="django~=5.0"),
         nox.param("5.2", id="django~=5.2"),
         nox.param("6.0", id="django~=6.0"),
     ],
